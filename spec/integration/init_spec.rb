e2638d76b0297b498981ffdf790e139717f48aab
Oleg Pudeyev <oleg@bsdpower.com>
2020-02-17 19:54:31 -0500

    assert topology type

--- spec/integration/init_spec.rb
+++ spec/integration/init_spec.rb
@@ -5,6 +5,14 @@ describe 'init' do
     MongoManager::Executor.new(options)
   end
 
+  let(:client_options) do
+    {server_selection_timeout: 5}
+  end
+
+  let(:client) do
+    Mongo::Client.new(['localhost:27017'], client_options)
+  end
+
   context 'single' do
     let(:options) do
       {
@@ -14,6 +22,9 @@ describe 'init' do
 
     it 'starts' do
       executor.init
+
+      client.database.command(ping: 1)
+      client.cluster.topology.class.name.should =~ /Single/
     end
   end
 end
